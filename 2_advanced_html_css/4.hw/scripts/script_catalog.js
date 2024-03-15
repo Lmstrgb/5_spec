3d6c6b82b2d02dbc2ff3dff0006a0f228b5aa7b1
Lmstrgb <lmstrmax@gmail.com>
2024-03-15 20:47:33 +0500

    4.hw_registration

---
+++ 2_advanced_html_css/4.hw/scripts/script_catalog.js
@@ -0,0 +1,7 @@
+ document.querySelector('.product').style.paddingBottom = '0px';
+
+// Получаем элемент с классом "product"
+// const productElement = document.querySelector('.product');
+
+// // Изменяем padding-bottom на 10px
+// productElement.style.paddingBottom = '0px';
