5cfaa44b971934e54ebf3b1a2b9cce325165e7a5
Lmstrgb <lmstrmax@gmail.com>
2024-04-01 16:37:58 +0500

    6.hw

--- 2_advanced_html_css/6.hw/scripts/script_cart.js
+++ 2_advanced_html_css/6.hw/scripts/script_cart.js
@@ -1,1 +1,9 @@
-document.querySelector('.footer').style.marginTop = '128px';
+//document.querySelector('.footer').style.marginTop = '128px';
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth <= 420) {
+      document.querySelector('.footer').style.marginTop = '96px';
+  } else {
+      document.querySelector('.footer').style.marginTop = '128px';
+  }
+});
