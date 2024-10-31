bf86e4ecfd01df5b85a287042a22d7cab9a3dff4
Lmstrgb <lmstrmax@gmail.com>
2024-10-31 22:05:55 +0300

    hw.3

--- 5_js_ecmascript/hw_3/readme.md
+++ 5_js_ecmascript/hw_3/readme.md
@@ -22,5 +22,26 @@ manager.displayInfo();
 // Name: Jane Doe
 // Department: Sales
 
-**Задание 2: ""Управление списком студентов""**
+**Задание 2: ""Управление списком заказов""**
 
+Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// Пример использования класса
+class Product {
+constructor(name, price) {
+this.name = name;
+this.price = price;
+}
+}
+
+const order = new Order(12345);
+const product1 = new Product(""Phone"", 500);
+order.addProduct(product1);
+const product2 = new Product(""Headphones"", 100);
+order.addProduct(product2);
+console.log(order.getTotalPrice()); // Вывод: 600
--- 5_js_ecmascript/hw_3/script.js
+++ 5_js_ecmascript/hw_3/script.js
@@ -42,8 +42,81 @@ class Manager extends Employee {
   }
 }
 
-const employee = new Employee("Ivan Shahmaev");
-employee.displayInfo();
+// const employee = new Employee("Ivan Shahmaev");
+// employee.displayInfo();
 
-const manager = new Manager("Eugene Igorev", "Отдел продаж");
-manager.displayInfo();
+// const manager = new Manager("Eugene Igorev", "Отдел продаж");
+// manager.displayInfo();
+
+
+// **Задание 2: ""Управление списком заказов""**
+
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+// Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// // Пример использования класса
+// class Product {
+// constructor(name, price) {
+// this.name = name;
+// this.price = price;
+// }
+// }
+
+// const order = new Order(12345);
+// const product1 = new Product(""Phone"", 500);
+// order.addProduct(product1);
+// const product2 = new Product(""Headphones"", 100);
+// order.addProduct(product2);
+// console.log(order.getTotalPrice()); // Вывод: 600
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+}
+
+class Order {
+  constructor(orderNumber) {
+    this.orderNumber = orderNumber;
+    this.products = [];
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+  }
+
+  getTotalPriceReduce() {
+    // text = "Цена всех продуктов = ";
+    return this.products.reduce((total, product) => total + product.price, 0);
+  }
+
+  getTotalPrice() {
+    let total = 0;
+    for (let elem of this.products) {
+      total += elem.price;
+    }
+    return total;
+  }
+
+  getLog() {
+    console.log("Общая цена = " + this.getTotalPrice().toString());
+    console.log("Общая цена = " + this.getTotalPriceReduce().toString() + ", использовался метод reduce");
+  }
+
+}
+
+// Пример использования класса
+const order = new Order(12345);
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+const product2 = new Product("Headphones", 200);
+order.addProduct(product2);
+
+// console.log(order.getTotalPrice()); // Вывод: 600
+order.getLog();
