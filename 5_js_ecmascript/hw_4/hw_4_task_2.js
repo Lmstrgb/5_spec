2e4381a002af6e9449cd8712088f3add1c0f92eb
Lmstrgb <lmstrmax@gmail.com>
2024-11-19 19:36:54 +0300

    hw_4

---
+++ 5_js_ecmascript/hw_4/hw_4_task_2.js
@@ -0,0 +1,43 @@
+console.log(`Задание 2`);
+const dogUrl = "https://dog.ceo/api/breeds/image/random";
+const dogImagesContainer = document.querySelector('div.dog_images');
+
+const getData2 = async (argUrl) => {
+  try {
+    // получение данных от сервера
+    const res = await fetch(argUrl);
+    //получене данных от res
+    const data = await res.json();
+    return data.message;
+  } catch (error) {
+    console.log('Ошибка при получении пользователей:', error);
+  }
+};
+//const fetchData = await getData2(dogUrl);
+// displayUsers(fetchData);
+console.log("----------------------");
+//console.log(fetchData);
+//displayDogImage(fetchData);
+
+// Функция для отображения изображения собаки
+function displayDogImage(url) {
+  const img = document.createElement('img');
+  img.src = url;
+  img.alt = 'Собака';
+  dogImagesContainer.appendChild(img);
+}
+
+
+
+// Функция для получения и отображения 10 изображений с интервалом в 3 секунды
+async function loadDogImages(urlArg) {
+  for (let i = 0; i < 10; i++) {
+    const fetchData = await getData2(dogUrl);
+    const imageUrl = fetchData;
+    displayDogImage(imageUrl);
+    await new Promise(resolve => setTimeout(resolve, 3000)); // Ждем 3 секунды
+  }
+}
+
+// Запускаем процесс загрузки изображений
+loadDogImages(dogUrl);
