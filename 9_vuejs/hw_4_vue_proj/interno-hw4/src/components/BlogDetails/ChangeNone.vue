9f54f652b1811c97095b68f2349e9c6e26e93484
Lmstrgb <lmstrmax@gmail.com>
2025-05-01 21:59:24 +0300

    hw_4_proj added

---
+++ 9_vuejs/hw_4_vue_proj/interno-hw4/src/components/BlogDetails/ChangeNone.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <change-kitchen />
+    <change-bedroom />
+    <change-building />
+    <change-architecture />
+    <change-planning />
+  </div>
+</template>
+
+<script>
+import ChangeKitchen from './ChangeKitchen.vue'
+import ChangeBedroom from './ChangeBedroom.vue'
+import ChangeBuilding from './ChangeBuilding.vue'
+import ChangeArchitecture from './ChangeArchitecture.vue'
+import ChangePlanning from './ChangePlanning.vue'
+
+export default {
+  name: 'ChangeNone',
+  components: {
+    ChangeKitchen,
+    ChangeBedroom,
+    ChangeBuilding,
+    ChangeArchitecture,
+    ChangePlanning
+  }
+}
+</script>
