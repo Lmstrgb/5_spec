88e3219e237f36fda8dc411a6049ccd3d08802d4
Lmstrgb <lmstrmax@gmail.com>
2025-05-10 18:57:02 +0300

    hw_6_vue_proj

---
+++ 9_vuejs/hw_6_vue_proj/interno-hw6/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <!-- <BlogDetailsMain /> -->
+     <AppHeader/>
+     <router-view/>
+     <AppFooter/>
+     <!-- <PageHome /> -->
+  </div>
+</template>
+
+<script>
+// import BlogDetailsMain from './components/Pages/BlogDetailsMain.vue'
+// import PageHome from './components/Pages/PageHome.vue';
+import AppFooter from './components/Universal/AppFooter.vue';
+import AppHeader from './components/Universal/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // BlogDetailsMain
+    // PageHome
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+/* Глобальные стили из styleDetails.css */
+@import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Jost&family=Lato:wght@300;400;700;900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Jost", "DM Serif Display";
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+@import '@/assets/styles/styleIndexBlog.css';
+/* Остальные глобальные стили из styleDetails.css */
+</style>
---
+++ 9_vuejs/hw_6_vue_proj/interno-hw6/src/components/BlogDetails/ChangeKitchen.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <p class="blogDetails__mid__content__right__text">Знание о том, как жить дальше, поглощает всю жизненную силу,
+    которая есть у человека.Вопреки распространенному мнению.Существует множество вариантов цитат из
+    "Lorem Ipsum", но большинство из них подверглись изменениям в той или иной форме, путем добавления юмора или
+    случайных слов, которые выглядят даже немного неправдоподобно. Неловкость, скрытая в середине текста.
+    Все генераторы Lorem Ipsum в Интернете, как правило, повторяют предопределенные фрагменты по мере необходимости.
+    </p>
+    <div class="blogDetails__mid__content__right__quotes">
+      <h1 class="blogDetails__mid__content__right__quotes__header">“</h1>
+      <p class="blogDetails__mid__content__right__quotes__text">Какая-то умная и красивая цитата.</p>
+    </div>
+    <h1 class="blogDetails__mid__content__right__header">!Кухня! Дизайнерские спринты - это здорово</h1>
+    <p class="blogDetails__mid__content__right__text">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem
+        vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of
+        Lorem Ipsum available, but the majority have suffered.</p>
+    <ul class="blogDetails__mid__content__right__points">
+      <ol class="blogDetails__mid__content__right__text"> <span
+              class="blogDetails__mid__content__right__points__text">1.</span>Contrary to popular
+          belief.There are many
+          variations of passages of Lorem Ipsum available, but
+          the majority have suffered.</ol>
+      <ol class="blogDetails__mid__content__right__text"><span
+              class="blogDetails__mid__content__right__points__text">2.</span>Contrary to popular
+          belief.There are many
+          variations of passages of Lorem Ipsum available, but
+          the majority have suffered.</ol>
+      <ol class="blogDetails__mid__content__right__text"><span
+              class="blogDetails__mid__content__right__points__text">3.</span>Contrary to popular
+          belief.There are many
+          variations of passages of Lorem Ipsum available, but
+          the majority have suffered.</ol>
+    </ul>
+
+    <img class="blogDetails__mid__content__right__img" src="@/assets/img/blogDetaisImg2.png" alt="photo">
+    <p class="blogDetails__mid__content__right__text">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem
+        vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of
+        Lorem Ipsum available, but the majority have suffered.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChangeKitchen',
+  props: ['postData']
+}
+</script>
+
+<style scoped>
+/* Стили для этого компонента */
+</style>
